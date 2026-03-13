fe903107323279ca1077fdebf85f181e1b5da1d4
miloszgad <miloszgad@onet.pl>
2026-03-13 19:52:54 +0100

    Finish my Project
    At first i changed files name to more readeable - sketch.js and style.css
    Moreover I created a mechanism off project - based on my knowledge - a sketch visible on the website where we can draw sth using `mouseover` method.
    Apart of that on the website now there is a button, which thanks to we can change squares size in the grid.
    
    Website should present simulation of colorful 'Etch A Sketch' game.

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const container = document.querySelector(".container")
+
+
+
+function createSquare() {
+    for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+    }
+}
+createSquare()
+
+
+
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+    square.addEventListener("mouseover", function (e) {
+        e.target.style.background = getRandomColor();
+    })
+});
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb( ${r}, ${g}, ${b})`;
+}
+
+
+ const gridChange = document.querySelector(".button");
+ gridChange.addEventListener("click", function () {
+    const value = prompt("Please enter your grid size (100=max)");
+
+    if (value <= 100) {
+     container.innerHTML = "";
+     for (let i = 0; i < value * value; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.style.height = 256 / value + "px";
+      square.style.width = 256 / value + "px";
+      container.appendChild(square);
+     }
+     const squares = document.querySelectorAll(".square");
+     squares.forEach((square) => {
+     square.addEventListener("mouseover", function (e) {
+        e.target.style.background = getRandomColor();
+    })
+})
+    }
+    else if (value > 100) {
+        alert("Sorry, but your number is too big")
+    }
+    else {
+        alert("This is not a right number");
+    }
+ });
+
+
